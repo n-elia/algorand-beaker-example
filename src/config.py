8b171cfbeefd4c8cfadccfd50548dfeec95d6427
Nicola <62206011+n-elia@users.noreply.github.com>
2022-10-18 01:58:26 +0200

    Adds a config file mechanism

---
+++ src/config.py
@@ -0,0 +1,31 @@
+import os
+from typing import Optional
+
+from pydantic import BaseSettings
+
+
+class Config(BaseSettings):
+    ###########################################################################
+    # General
+    ###########################################################################
+
+    ###########################################################################
+    # Logging
+    ###########################################################################
+    LOG_LEVEL: Optional[str] = None
+    LOG_FORMAT: Optional[str] = None
+
+    ###########################################################################
+    # Test
+    ###########################################################################
+    TEST_SANDBOX_CONFIG: str = 'dev'  # dev, testnet
+    TEST_SANDBOX_WALLET_NAME: Optional[str] = None  # only for testnet
+    TEST_SANDBOX_WALLET_PASS: Optional[str] = None  # only for testnet
+
+    class Config:
+        case_sensitive = False
+        env_file = os.getenv('DOT_ENV_FILE', 'src/.env')
+        env_file_encoding = os.getenv('DOT_ENV_FILE_ENCODING', 'utf-8')
+
+
+current_config = Config()
